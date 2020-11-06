5ca21b6049a1cc3767546a6423a89f687fcdd815
Adam Wright <adam.t.wright@outlook.com>
2020-11-06 19:44:53 +0000

    remove use of Rigidbody2D in place of Transform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,32 +4,48 @@ public class PlayerController : MonoBehaviour
 {
     public int Speed;
     public int JumpHeight;
-    public Transform GroundCheck;
+    public LayerMask Ground;
 
-    private Rigidbody2D Body;
-    private Vector2 Inputs;
+    private Vector2 Velocity;
     private bool IsGrounded;
+    private RaycastHit2D[] hits;
 
-    void Start()
-    {
-        Body = GetComponent<Rigidbody2D>();
-    }
+    /*
+     *  s = ut + 0.5 * a * t^2
+     *  s = ut if a = 0
+     *  v^2 = u^2 + 2 * a * s
+     *  - 2 * a * s = u^2
+     *  sqrt(-2 * g * s) = u
+     */
 
     void Update()
     {
-        IsGrounded = Physics2D.OverlapBox(GroundCheck.position, new Vector2(1, 1), 0);
+        IsGrounded = false;
 
-        Inputs = Vector2.zero;
-        Inputs.x = Input.GetAxis("Horizontal");
+        hits = Physics2D.BoxCastAll(new Vector2(transform.position.x, transform.position.y), new Vector2(1, 1), 0f, Vector2.down, 1f, Ground);
 
-        if (Input.GetButtonDown("Jump") && IsGrounded)
+        if (hits.Length > 0)
         {
-            Body.AddForce(Vector2.up * Mathf.Sqrt(JumpHeight * -2f * Physics2D.gravity.y), ForceMode2D.Impulse);
+            IsGrounded = true;
+            Debug.Log(IsGrounded);
         }
-    }
 
-    void FixedUpdate()
-    {
-        Body.MovePosition(Body.position + Inputs * Speed * Time.fixedDeltaTime);
+        Velocity.x = Input.GetAxisRaw("Horizontal") * Speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Velocity.y += Mathf.Sqrt(-2 * Physics2D.gravity.y * JumpHeight);
+        }
+
+        if (IsGrounded)
+        {
+            Velocity.y = 0;
+        }
+        else
+        {
+            Velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        }
+
+        transform.Translate(Velocity * Time.deltaTime);
     }
 }
