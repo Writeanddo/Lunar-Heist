5e1d3e1870dff457354f408bf6468aef61555ab8
Megan <megan@theparkes.me.uk>
2020-11-11 22:35:51 +0000

    plugin play button

---
+++ Assets/Scripts/menus/StartMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("MenuOverlay", LoadSceneMode.Single);
+    }
+}
