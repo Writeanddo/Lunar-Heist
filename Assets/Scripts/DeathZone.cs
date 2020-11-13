b7342677111f3349ba93608cb152d65bf83077a7
Megan <megan@theparkes.me.uk>
2020-11-13 19:08:43 +0000

    respawn scout after death

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DeathZone : MonoBehaviour
+{
+    public Respawner Respawner;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Respawner.Respawn();
+        }
+    }
+}
---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+    public GameObject[] Zones;
+    public GameObject Player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Respawn()
+    {
+        Vector2 respawnPosition = Zones[0].gameObject.transform.position;
+        Player.transform.position = new Vector3(respawnPosition.x, respawnPosition.y, Player.transform.position.z);
+    }
+}
