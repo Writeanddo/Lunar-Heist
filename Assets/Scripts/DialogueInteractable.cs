b9c45d9dbf7b0723ad486f06b0f456109ffd27ff
Megan <megan@theparkes.me.uk>
2020-11-17 22:43:52 +0000

    more tiles and add text mesh pro

---
+++ Assets/Scripts/DialogueInteractable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueInteractable : MonoBehaviour
+{
+    public string text;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            if (Input.GetButtonUp("Submit"))
+            {
+                Debug.Log("enter");
+            }
+            Debug.Log("dialogue interactable");
+
+            col.gameObject.GetComponentInChildren<PlayerDialogue>().setText(text);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerDialogue.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class PlayerDialogue : MonoBehaviour
+{
+
+    public TextMeshPro Text;
+    
+
+
+    void Start()
+    {
+        Text.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void setText(string text)
+    {
+        Text.text = text;
+
+    }
+}
