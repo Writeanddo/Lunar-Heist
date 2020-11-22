4b0523b555dce9806fd0c1ac69a427f160415b4e
Adam Wright <adam.t.wright@outlook.com>
2020-11-22 20:16:15 +0000

    move player with platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -2,23 +2,43 @@
 
 public class Platform : MonoBehaviour
 {
+    public BoxCollider2D BoxCollider;
+
     private Transform oldParent;
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void FixedUpdate()
     {
-        if (collision.tag == "Player")
+        Collider2D[] OverlappingColliders = Physics2D.OverlapBoxAll(BoxCollider.transform.position, BoxCollider.size, 0);
+
+        for (int i = 0; i < OverlappingColliders.Length; i++)
         {
-            oldParent = collision.transform.parent;
-            collision.transform.SetParent(transform);
+            Collider2D collider = OverlappingColliders[i];
+
+            if (collider.tag == "Player")
+            {
+                Rigidbody2D rb2d = collider.gameObject.GetComponent<Rigidbody2D>();
+                ColliderDistance2D distance = collider.Distance(BoxCollider);
+
+                rb2d.position = rb2d.position + distance.normal * distance.distance;
+            }
         }
     }
 
+    // void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     if (collision.tag == "Player")
+    //     {
+    //         oldParent = collision.transform.parent;
+    //         collision.transform.SetParent(transform);
+    //     }
+    // }
 
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            collision.transform.SetParent(oldParent);
-        }
-    }
+
+    // void OnTriggerExit2D(Collider2D collision)
+    // {
+    //     if (collision.tag == "Player")
+    //     {
+    //         collision.transform.SetParent(oldParent);
+    //     }
+    // }
 }
