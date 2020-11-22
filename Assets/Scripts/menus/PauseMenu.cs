71f2b378b4a404b1590f67a3567793eed7f75d64
Megan <megan@theparkes.me.uk>
2020-11-22 21:58:10 +0000

    add basic pause menu

---
+++ Assets/Scripts/menus/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Menu;
+    void Update()
+    {
+        if (Input.GetButtonUp("Pause")){
+            Debug.Log("Pause");
+            Menu.SetActive(!Menu.activeSelf);
+        }
+    }
+
+    public void Resume()
+    {
+        Menu.SetActive(false);
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
