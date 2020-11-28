f8d6715d20beeec739e988c71caea2403b4e05be
Megan <megan@theparkes.me.uk>
2020-11-28 10:38:43 +0000

    opening cutscene wip

---
+++ Assets/Scripts/menus/Cutscene.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class Cutscene : MonoBehaviour
+{
+    public List<string> Dialogue;
+    public List<Color> WhoIsSpeaking;
+    public int textIndex = -1;
+    public GameObject TextContainer;
+    public TextMeshProUGUI Text;
+    public string NextScene;
+    public Image Panel;
+
+    private bool fadingOut;
+
+    void Update()
+    {
+        if (!fadingOut && Input.GetButtonUp("Fire1"))
+        {
+            Next();
+        }
+
+     
+    }
+
+    void FixedUpdate()
+    {
+        if (fadingOut)
+        {
+            Panel.color = new Color(Panel.color.r, Panel.color.g, Panel.color.b, Panel.color.a - 0.1f);
+
+            if (Panel.color.a <= 0)
+            {
+                SceneManager.LoadScene(NextScene);
+            }
+        }
+    }
+
+    private void Next()
+    {
+        if (!TextContainer.activeSelf)
+        {
+            textIndex = 0;
+            TextContainer.SetActive(true);
+            setText();
+        }
+        else if (textIndex == Dialogue.Count - 1)
+        {
+            fadingOut = true;
+            TextContainer.SetActive(false);
+        }
+        else
+        {
+            textIndex += 1;
+            setText();
+        }
+    }
+
+    private void setText()
+    {
+        Text.text = "\n" + Dialogue[textIndex];
+        Text.color = WhoIsSpeaking[textIndex];
+    }
+}
