0369b59c6f6ec504b5cf8894529da5aa379b3be7
Halden <halden@theparkes.me.uk>
2020-11-29 13:42:57 +0000

    added running sound

---
+++ Assets/PieSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieSound : MonoBehaviour
+{
+    public AudioSource runSoundSouce;
+    public AudioClip[] runningSound;
+    public void StartRunSound()
+    {
+        runSoundSouce.PlayOneShot(runningSound.PickRandom());
+    }
+}
