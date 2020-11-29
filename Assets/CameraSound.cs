1d7b0a1d3561cfa796a0a4a9b9557400a6074ec2
Halden <halden@theparkes.me.uk>
2020-11-29 15:47:00 +0000

    camera animation sound added

---
+++ Assets/CameraSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSound : MonoBehaviour
+{
+    public AudioSource cameraSoundSouce;
+    public AudioClip[] cameraSound;
+    public void TurnSound()
+    {
+        cameraSoundSouce.PlayOneShot(cameraSound.PickRandom());
+    }
+}
