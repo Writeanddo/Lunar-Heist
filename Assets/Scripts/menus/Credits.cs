fb28123028134e078a6d973be0582df169885b2f
Halden <halden@theparkes.me.uk>
2020-11-29 17:46:43 +0000

    prevent clipping on credits

--- Assets/Scripts/menus/Credits.cs
+++ Assets/Scripts/menus/Credits.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    public AudioSource creditsMusic;
+
     void Update()
     {
         if (Input.GetButtonUp("Fire1"))
         {
+            creditsMusic.volume = 0;
             SceneManager.LoadScene("StartMenu");
         }
     }
