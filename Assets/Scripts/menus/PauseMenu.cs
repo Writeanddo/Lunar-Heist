4a958ee52db9542a431ed140190b6372c44e48eb
Megan <megan@theparkes.me.uk>
2020-11-30 16:40:51 +0000

    add controls to pause screen

--- Assets/Scripts/menus/PauseMenu.cs
+++ Assets/Scripts/menus/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour
     public AudioMixer Mixer;
 
     public GameObject Menu;
+    public GameObject ControlsMenu;
     void Update()
     {
         if (Input.GetButtonUp("Pause")){
@@ -23,6 +24,7 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         Menu.SetActive(true);
+        ControlsMenu.SetActive(false);
         Time.timeScale = 0;
         Mixer.SetFloat("MasterCutoffFrequencyLowPass", 250f);
         Mixer.SetFloat("MasterResonanceLowPass", 0.1f);
@@ -37,6 +39,11 @@ public class PauseMenu : MonoBehaviour
 
     }
 
+    public void Controls()
+    {
+        ControlsMenu.SetActive(true);
+        Menu.SetActive(false);
+    }
 
     public void Quit()
     {
