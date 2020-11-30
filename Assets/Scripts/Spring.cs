69d54854ffcac2842f01a2c388971da406bc4c73
Halden <halden@theparkes.me.uk>
2020-11-30 13:04:39 +0000

    added scout box spring sound

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -7,11 +7,15 @@ public class Spring : MonoBehaviour
     public int XVelocity;
     public float YVelocity;
 
+    public AudioSource bounceSoundSource;
+    public AudioClip[] bounceSound;
+
     void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Box")
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Force, ForceMode2D.Impulse);
+            bounceSoundSource.PlayOneShot(bounceSound.PickRandom());
         } 
         else if (collision.tag == "Player")
         {
@@ -19,6 +23,7 @@ public class Spring : MonoBehaviour
 
             playerController.Velocity = new Vector2(playerController.Velocity.x, YVelocity);
             playerController.Speed =  XVelocity;
+            bounceSoundSource.PlayOneShot(bounceSound.PickRandom());
         }
     }
 }
