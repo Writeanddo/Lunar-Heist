fd018edd3ff69992acb5d6bbc7a94d40120e6dd6
Megan <megan@theparkes.me.uk>
2020-11-30 11:35:19 +0000

    scout blocker

--- Assets/blocker/BlockerManager.cs
+++ Assets/blocker/BlockerManager.cs
@@ -6,7 +6,8 @@ public class BlockerManager : MonoBehaviour
 {
     public Dictionary<BlockerName, bool> Blockers = new Dictionary<BlockerName, bool>(){
         { BlockerName.WYNN_DOOR_1, false },
-        { BlockerName.PIE_UMBRELLA_1, false }
+        { BlockerName.PIE_UMBRELLA_1, false },
+        { BlockerName.SCOUT_DRAWBRIDGE, false }
     };
 
     public void FlipBlocker(BlockerName name)
--- Assets/blocker/BlockerName.cs
+++ Assets/blocker/BlockerName.cs
@@ -1,5 +1,6 @@
 ﻿public enum BlockerName
 {
     WYNN_DOOR_1,
-    PIE_UMBRELLA_1
+    PIE_UMBRELLA_1,
+    SCOUT_DRAWBRIDGE
 }
